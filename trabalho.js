623ce9883545269148592ca2e86c8fb5996ffee3
Mateus Antônio Pereira Gomes <mateus_antonio3@hotmail.com>
2018-11-26 04:51:00 -0200

    Aplicação do conceito de grid do bootstrap e leitura de mais de um arquivo para dentro das divs criadas Sono demais para comentar o código agora, amanhã faço isso e explico o que fiz pra qm quiser ouvir

--- trabalho.js
+++ trabalho.js
@@ -1,14 +1,20 @@
-
 var openFile = function(event) {
-    var input = event.target;
+    var input = event.target.files;
 
-    var reader = new FileReader();
-    reader.onload = function() {
-        var text = reader.result;
-        var node = document.getElementById('output');
-        node.innerText = text;
-        $('.hide').css({display:'block'});  //mostra a caixa de saida na tela
-        console.log(reader.result.substring(0, 200));
-    };
-    reader.readAsText(input.files[0]);
+    Object.keys(input).forEach(file => {
+        var reader = new FileReader();
+        reader.readAsText(input[file]);
+        reader.onload = function() {
+            var text = reader.result;
+            var iDiv = document.createElement("div");
+            iDiv.setAttribute("id", "file_" + file);
+            iDiv.classList.add("saida", "test");
+            $('#content').append(iDiv);
+            var node = document.getElementById("file_" + file);
+            node.innerText = text;
+            // $('#output_id').text(text); //Faz exatamente o que as duas linhas acima fazem juntas, porém, por algum motivo está printando tudo sem quebra de linhas
+            $('.saida').css({ display: "block" }); //mostra a caixa de saida na tela
+            console.log(reader.result.substring(0, 200)); //Só printa isso no console.
+        };
+    });
 };
