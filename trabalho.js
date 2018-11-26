2c23ac3d450aef861f0d4e2626f2aff3c4c4676b
Elciney <elcineyjr123@gmail.com>
2018-11-26 14:57:15 -0200

    trim funcionando corretamente + comentarios

--- trabalho.js
+++ trabalho.js
@@ -11,32 +11,32 @@ var openFile = function(event) {
             iDiv.classList.add("saida", "test");
             $('#content').append(iDiv);
             var node = document.getElementById("file_" + file);
-            // node.innerText = text;
-            // $('#output_id').text(text); //Faz exatamente o que as duas linhas acima fazem juntas, porém, por algum motivo está printando tudo sem quebra de linhas
-            // $('.saida').css({ display: "block" }); //mostra a caixa de saida na tela
-            // console.log(reader.result.substring(0, 200)); //Só printa isso no console.
-
-            // Criação dos relatórios
             
+            // Criação dos relatórios
             var lines = text.split('\n');
             var totalVagas = 0;
             var elected = '';
             node.innerHTML = 'Numero de vagas: ';
             for (let i = 1; i < lines.length; i++) {
-                let line = lines[i].split(';');
-                if(line[0].includes('*')) {
-                    elected += i + ' - ' + line[2];
-                    let partido = line[3].split('-');
-                    partido[0].trim();
-                    elected += ' (' + partido[0] + ', ' + line[4] + 'votos)';
-                    if(partido.length > 1) elected +=  '- Coligação: ' + partido[1];
+                let line = lines[i].split(';'); //divide a linha pelo ';'
+
+                if(line[0].includes('*')) { //entra no if se houver o '*' antes do candidato
+
+                    elected += i + ' - ' + line[2]; //adiciona o contador e o nome do candidato na string de saida
+
+                    let partido = line[3].split('-'); //divide a string pelo '-' pra separar partido da coligacao
+                    partido[0] = partido[0].trim(); //remove os espacos do inicio e final
+
+                    elected += ' (' + partido[0] + ', ' + line[4] + ' votos) '; //adiciona o partido do cadidato e o numero de votos na string de saida
+
+                    if(partido.length > 1) elected +=  '- Coligação: ' + partido[1]; //se o partido tiver coligacoes
                     elected += '<br>';
                     totalVagas++;
                 } else break;
             }
             node.innerHTML += totalVagas + '<br><br>Vereadores eleitos:<br>';
             node.innerHTML += elected;
-            $('.saida').css({ display: "block" });
+            $('.saida').css({ display: "block" }); //mostra a caixa de saida na tela
         };
     });
 };
